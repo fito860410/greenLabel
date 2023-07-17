a63d05343a63baf2397a8cdde7c2ba0f5b9e8c8f
Frangel Moreno Díaz <frangeldeveloper@gmail.com>
2023-07-16 19:27:23 -0600

    Proceso de busqueda

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,3 +1,5 @@
+import { CarProps } from "@/types";
+
 export async function fecthCars() {
     const headers = {
         'X-Api-Key': 
@@ -5,7 +7,7 @@ export async function fecthCars() {
         'X-Api-Host': 'api.api-ninjas.com'
     }
 
-    const response = await fetch('https://api.api-ninjas.com/v1/cars?model=camry', {
+    const response = await fetch('https://api.api-ninjas.com/v1/cars?model=carrera', {
         headers: headers,        
     });
 
@@ -25,4 +27,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageFactor + ageRate;
 
     return rentalRatePerDay.toFixed(0);
+}
+
+export const generateCarImageUrl = (car: CarProps, angle ? : string ) => {
+    const url = new URL('https://cdn.imagin.studio/getimage');
+
+    const {make, year, model} = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(' ')[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);    
+
+    return `${url}`    
 }
